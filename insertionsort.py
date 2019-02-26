026729672c4cb62b7e42dc3bb910037d62b9180e
Becka Henry <36617288+beckahenryl@users.noreply.github.com>
2019-02-26 10:28:59 -0800

    Add files via upload
    insertion sort array of integers in ascending order

---
+++ insertionsort.py
@@ -0,0 +1,20 @@
+'''
+use the insertion sort algorithm to sort
+through the array in ascending order
+'''
+array = [4,3,2,10,12,1,5,6]
+
+def insertionSort (array):
+	for i in range(len(array)):
+		key = array[i]
+		j = i-1
+		while(j >= 0 and array[j] > key):
+			array[j+1] = array[j]
+			j = j-1
+			array[j+1] = key
+					
+
+insertionSort(array)
+
+for i in range(len(array)):
+	print (array[i])				
