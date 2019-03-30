ee95faf42e4b14993b3a2e7f744e6864451d93d9
unknown <rebek.henry7@gmail.com>
2019-03-30 11:08:57 -0700

    Add existing file

---
+++ queuenotes.py
@@ -0,0 +1,88 @@
+# FIFO Queue
+
+import json
+import requests
+
+# Use a queue to load in three of your favorite movies and then plug in your downtown city to find the closest movie theater using the fandango api
+
+# TMDB API Methods
+
+# Google Maps API Methods
+
+# Fandango Methods
+
+# Get - your requesting information from a server
+# Push - sending information to a server
+# Update - updating a value on a server
+# Delete - deleting a value on a server
+
+# Requesting Information from TMDB API
+
+# def getMovie(query):
+
+  # print (qu
+
+  # https://docs.python.org/3/library/json.html
+
+
+
+# array/list/collection fifo queue 
+
+
+
+# [1 , 2, 3]
+# push(2)
+# [2,1,2,3]
+# pop()
+
+
+# []
+
+
+# class Stack
+
+
+
+#  class Queue:
+
+  #  def __init__(self):
+
+
+class Stack:
+  def __init__(self, collection):
+     self.collection = collection
+     if self.collection.len() == 0:
+      self.head = None
+      self.tail = None
+     else if self.collection.len() == 1:
+      self.head = self.collection[0]
+     else
+      self.tail = self.collection[self.collection.len() - 1] 
+     
+  def isEmpty():
+    return head == None
+  
+  def push(data):
+    return data
+
+  def pop():
+    if (isEmpty()):
+      self.collection.pop
+    else:
+      print("error parsing collection from stack")
+
+
+
+# Head <--- 1 [1, 2, 3] 3 --> tail
+
+stack = Stack([1,2,3])
+stack.pop()
+
+stackOne = Stack([])
+
+
+print(stack.collection)
+
+
+
+
